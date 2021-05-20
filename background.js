d84f8387a82e2a30ecaadb0188ec447864ba91a9
viiirus07 <viiirus07@gmail.com>
2021-05-20 14:07:34 +0530

    Added linking

--- background.js
+++ background.js
@@ -4,6 +4,15 @@ chrome.contextMenus.create({
     onclick: myFunction
 });
 
-function myFunction() {
-    alert("dddddd");
+function myFunction(selectedText) {
+    const tweetContent = selectedText.selectionText;
+    if (tweetContent.length <= 280) {
+        const tweetUrl = "https://twitter.com/intent/tweet?text=" + selectedText.selectionText;
+        chrome.tabs.create({
+            url: tweetUrl
+        });
+    } else {
+        alert("The maximum characters allowed in a tweet is 280");
+    }
+
 }
